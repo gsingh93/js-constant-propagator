6736177f747866b4f630d2d72a4124209c965b95
Gulshan Singh <gsingh2011@gmail.com>
2015-10-05 02:43:11 -0700

    Added visit_attr and made most visitors use that

--- constant-propagator.py
+++ constant-propagator.py
@@ -8,6 +8,10 @@ from slimit.visitors.nodevisitor import ASTVisitor
 from slimit import ast
 
 
+def print_indented(indent, node):
+    print indent * ' ' + type_name(node)
+
+
 def type_name(root):
     return type(root).__name__
 
@@ -24,13 +28,47 @@ class ConstantReductionVisitor(ASTVisitor):
     def __init__(self, debug):
         self.debug = debug
 
-    def visit_node(self, node):
-        children = node.children()
-        for i in range(len(children)):
-            if children[i] != None:
-                children[i] = self.visit(children[i])
+    def visit_attrs(self, node, *attrs):
+        if node is None:
+            return None
+
+        for attr_name in attrs:
+            if not hasattr(node, attr_name):
+                raise Exception("Property %s doesn't exist on node type %s"
+                                % (attr_name, type_name(node)))
+            attr = getattr(node, attr_name, None)
+            if isinstance(attr, list):
+                for i in range(len(attr)):
+                    attr[i] = self.visit(attr[i])
+            elif isinstance(attr, ast.Node):
+                attr = self.visit(attr)
+            else:
+                continue
+
+            setattr(node, attr_name, attr)
 
         return node
+
+    def visit_array(self, node):
+        return self.visit_attrs(node, 'items')
+
+    def visit_regex(self, node):
+        return node
+
+    def visit_node(self, node):
+        return self.visit_attrs(node, '_children_list')
+
+    def visit_functioncall(self, node):
+        return self.visit_attrs(node, 'identifier', 'args')
+
+    def visit_newexpr(self, node):
+        return self.visit_attrs(node, 'identifier', 'args')
+
+    def visit_dotaccessor(self, node):
+        return self.visit_attrs(node, 'node', 'identifier')
+
+    def visit_return(self, node):
+        return self.visit_attrs(node, 'expr')
 
     def visit_identifier(self, node):
         if node.value in self.const_vars:
@@ -42,16 +80,83 @@ class ConstantReductionVisitor(ASTVisitor):
         node.value = node.value.decode('string_escape')
         return node
 
+    def visit_number(self, node):
+        return node
+
+    def visit_assign(self, node):
+        return self.visit_attrs(node, 'op', 'left', 'right')
+
+    def visit_object(self, node):
+        return self.visit_attrs(node, 'properties')
+
+    def visit_varstatement(self, node):
+        return self.visit_node(node)
+
+    def visit_block(self, node):
+        return self.visit_node(node)
+
     def visit_bracketaccessor(self, node):
-        node.expr = self.visit(node.expr)
         ident = node.node
         if self.debug:
-            print self.indent * ' ' + type_name(ident)
+            print_indented(self.indent + 1, ident)
+        node.expr = self.visit(node.expr)
 
         if type_name(ident) == 'Identifier':
             if ident.value in self.const_arrs and is_const(node.expr):
                 if node.expr.value in self.const_arrs[ident.value]:
                     return self.const_arrs[ident.value][node.expr.value]
+
+        return node
+
+    def visit_with(self, node):
+        return self.visit_attrs(node, 'expr', 'statement')
+
+    def visit_switch(self, node):
+        return self.visit_attrs(node, 'expr', 'cases', 'default')
+
+    def visit_case(self, node):
+        return self.visit_attrs(node, 'expr', 'elements')
+
+    def visit_defaults(self, node):
+        return self.visit_attrs(node, 'elements')
+
+    def visit_label(self, node):
+        return self.visit_attrs(node, 'identifier', 'statement')
+
+    def visit_throw(self, node):
+        return self.visit_attrs(node, 'expr')
+
+    def visit_try(self, node):
+        return self.visit_attrs(node, 'statements', 'catch', 'fin')
+
+    def visit_catch(self, node):
+        return self.visit_attrs(node, 'identifier', 'elements')
+
+    def visit_finally(self, node):
+        return self.visit_attrs(node, 'elements')
+
+    def visit_funcbase(self, node):
+        return self.visit_attrs(node, 'identifier', 'parameters', 'elements')
+
+    def visit_funcdecl(self, node):
+        return self.visit_funcbase(node)
+
+    def visit_funcexpr(self, node):
+        return self.visit_funcbase(node)
+
+    def visit_comma(self, node):
+        return self.visit_attrs(node, 'left', 'right')
+
+    def visit_emptystatement(self, node):
+        return node
+
+    def visit_exprstatement(self, node):
+        return self.visit_attrs(node, 'expr')
+
+    def visit_ellision(self, node):
+        return node
+
+    def visit_this(self, node):
         return node
 
     def visit_vardecl(self, node):
@@ -59,7 +164,7 @@ class ConstantReductionVisitor(ASTVisitor):
 
         ident = node.identifier
         if self.debug:
-            print self.indent * ' ' + type_name(ident)
+            print_indented(self.indent + 1, ident)
         assert type_name(ident) == "Identifier"
 
         node.initializer = self.visit(node.initializer)
@@ -81,6 +186,12 @@ class ConstantReductionVisitor(ASTVisitor):
 
         return node
 
+    def visit_program(self, node):
+        return self.visit_node(node)
+
+    def visit_unaryop(self, node):
+        return self.visit_attrs(node, 'op', 'value', 'postfix')
+
     def visit_binop(self, node):
         node.left = self.visit(node.left)
         node.right = self.visit(node.right)
@@ -100,11 +211,43 @@ class ConstantReductionVisitor(ASTVisitor):
 
         return node
 
+    def visit_conditional(self, node):
+        return self.visit_attrs(node, 'predicate', 'consequent', 'alternative')
+
+    def visit_if(self, node):
+        return self.visit_attrs(node, 'predicate', 'consequent', 'alternative')
+
+    def visit_dowhile(self, node):
+        return self.visit_attrs(node, 'predicate', 'statement')
+
+    def visit_while(self, node):
+        return self.visit_attrs(node, 'predicate', 'statement')
+
+    def visit_for(self, node):
+        return self.visit_attrs(node, 'init', 'cond', 'count', 'statement')
+
+    def visit_forin(self, node):
+        return self.visit_attrs(node, 'item', 'iterable', 'statement')
+
+    def visit_continue(self, node):
+        return self.visit_attrs(node, 'identifier')
+
+    def visit_break(self, node):
+        return self.visit_attrs(node, 'identifier')
+
+    def visit_null(self, node):
+        return node
+
     def visit(self, root):
+        if root is None:
+            return None
+
         self.indent += 1
-        func = getattr(self, "visit_%s" % type_name(root).lower(), self.visit_node)
+        func = getattr(self, "visit_%s" % type_name(root).lower(), None)
+        if func is None:
+            raise Exception('No visitor method defined for type ' + type_name(root))
         if self.debug:
-            print self.indent * ' ' + type_name(root)
+            print_indented(self.indent, root)
         ret_val = func(root)
         self.indent -= 1
         return ret_val
